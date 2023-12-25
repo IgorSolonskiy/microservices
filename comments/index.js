95199eb797dcdaf8206c4702383c064c23f6398c
solonskiy <solonskiyigor@gmail.com>
2023-12-25 18:17:07 +0200

    add event routes

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const {randomBytes} = require('crypto');
+const axios = require('axios');
 
 const app = express();
 app.use(cors())
@@ -14,7 +15,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const {content} = req.body;
     const comment = {id, content};
@@ -25,7 +26,21 @@ app.post('/posts/:id/comments', (req, res) => {
 
     commentsByPostId[req.params.id] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            ...comment,
+            postId: req.params.id
+        }
+    })
+
     res.status(201).json(comments);
+})
+
+app.post('/events',(req,res)=>{
+    console.log('Received event', req.body.type)
+
+    res.send({});
 })
 
 app.listen(4001, () => {
