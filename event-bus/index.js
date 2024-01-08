fe2154c583c69b90e608e761ab244d208557771b
solonskiy <solonskiyigor@gmail.com>
2024-01-08 15:08:25 +0200

    added query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,15 +6,16 @@ const app = express();
 
 app.use(bodyParser.json())
 
-app.post('/events', async (req, res) => {
+app.post('/events', (req, res) => {
     const event = req.body;
 
-    await axios.post('http://localhost:4000/events', event);
-    await axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4001/events', event);
+    // axios.post('http://localhost:4003/events', event);
 
     res.send({status: 'OK'})
 })
 
-app.listen(4005,()=>{
+app.listen(4005, () => {
     console.log('Listening on 4005')
 })
