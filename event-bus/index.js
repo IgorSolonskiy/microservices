2ff327eca289936a380a8cdbd034a96fa6115e93
solonskiy <solonskiyigor@gmail.com>
2024-01-09 15:14:01 +0200

    added promise

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,10 +13,12 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-  axios.post("http://localhost:4003/events", event);
+  Promise.allSettled([
+    axios.post("http://localhost:4000/events", event),
+    axios.post("http://localhost:4001/events", event),
+    axios.post("http://localhost:4002/events", event),
+    axios.post("http://localhost:4003/events", event),
+  ]);
 
   res.send({ status: "OK" });
 });
